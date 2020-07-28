8ef51bd2f8e10bf6dfcae6cc81d65b92756d12c1
Eivind Gussiås Løkseth <eloekset@msn.com>
2020-07-28 20:31:53 +0200

    Add unit tests for ApplicationService_AbsoluteUrl

---
+++ test/Piranha.AspNetCore.Tests/Services/ApplicationService_AbsoluteUrlShould.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Copyright (c) .NET Foundation and Contributors
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * http://github.com/piranhacms/piranha
+ *
+ */
+
+using Piranha.AspNetCore.Services;
+using Piranha.Repositories;
+using Piranha.Services;
+using Shouldly;
+using Xunit;
+
+namespace Piranha.AspNetCore.Tests.Services
+{
+    public class ApplicationService_AbsoluteUrlShould : BaseTests
+    {
+        private ApplicationService _applicationService;
+        private IApi _api;
+
+        public ApplicationService_AbsoluteUrlShould()
+            : base()
+        {
+            _applicationService = new ApplicationService(_api);
+        }
+
+        /// <summary>
+        /// Sets up & initializes the tests.
+        /// </summary>
+        protected override void Init()
+        {
+            _api = CreateApi();
+            Piranha.App.Init(_api);
+        }
+
+        /// <summary>
+        /// Cleans up any possible data and resources
+        /// created by the test.
+        /// </summary>
+        protected override void Cleanup() 
+        {
+            _api.Dispose();
+            _api = null;
+            _applicationService = null;
+        }
+
+        private IApi CreateApi()
+        {
+            var factory = new ContentFactory(services);
+            var serviceFactory = new ContentServiceFactory(factory);
+
+            var db = GetDb();
+
+            return new Api(
+                factory,
+                new AliasRepository(db),
+                new ArchiveRepository(db),
+                new Piranha.Repositories.MediaRepository(db),
+                new PageRepository(db, serviceFactory),
+                new PageTypeRepository(db),
+                new ParamRepository(db),
+                new PostRepository(db, serviceFactory),
+                new PostTypeRepository(db),
+                new SiteRepository(db, serviceFactory),
+                new SiteTypeRepository(db)
+            );
+        }
+
+        [Fact]
+        public void AppendPathBase_ForVirtualDirectory()
+        {
+            string slug = "/";
+            string expectedUrl = @"http://localhost:5000/virtual-directory/";
+            TestData.PopulateRequestHelperFor_Localhost_Port5000_VirtualDirectory_NoSite(_applicationService);
+            string actualUrl = _applicationService.AbsoluteUrl(slug);
+            actualUrl.ShouldNotBeNullOrWhiteSpace("AbsoluteUrl generated some value");
+            actualUrl.ShouldBe(expectedUrl, "AbsoluteUrl generated the expected value");
+        }
+
+        [Fact]
+        public void NotAppendPathBase_WhenNotHostedInVirtualDirectory()
+        {
+            string slug = "/";
+            string expectedUrl = @"http://localhost:5000/";
+            TestData.PopulateRequestHelperFor_Localhost_Port5000_NoSite(_applicationService);
+            string actualUrl = _applicationService.AbsoluteUrl(slug);
+            actualUrl.ShouldNotBeNullOrWhiteSpace("AbsoluteUrl generated some value");
+            actualUrl.ShouldBe(expectedUrl, "AbsoluteUrl generated the expected value");
+        }
+    }
+}
---
+++ test/Piranha.AspNetCore.Tests/TestData.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Copyright (c) .NET Foundation and Contributors
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ *
+ * http://github.com/piranhacms/piranha
+ *
+ */
+
+using Piranha.AspNetCore.Services;
+
+namespace Piranha.AspNetCore.Tests
+{
+    internal class TestData
+    {
+        public const string SchemeHttp = "http";
+        public const string SchemeHttps = "https";
+        public const string HostLocalhost = "localhost";
+        public const int Port5000 = 5000;
+        public const string PathBaseVirtualDirectory = @"/virtual-directory";
+
+        public static void PopulateRequestHelperFor_Localhost_Port5000_VirtualDirectory_NoSite(IApplicationService service)
+        {
+            service.Request.Scheme = SchemeHttp;
+            service.Request.Host = HostLocalhost;
+            service.Request.Port = Port5000;
+            service.Request.PathBase = PathBaseVirtualDirectory;
+            service.Request.Url = CreateUrlForRequest(service.Request);
+        }
+
+        public static void PopulateRequestHelperFor_Localhost_Port5000_NoSite(IApplicationService service)
+        {
+            service.Request.Scheme = SchemeHttp;
+            service.Request.Host = HostLocalhost;
+            service.Request.Port = Port5000;
+            service.Request.PathBase = null;
+            service.Request.Url = CreateUrlForRequest(service.Request);
+        }
+
+        public static string CreateUrlForRequest(IRequestHelper request)
+        {
+            string port = request.Port.HasValue ? (":" + request.Port.Value) : string.Empty;
+            return $"{request.Scheme}://{request.Host}{port}{request.PathBase}";
+        }
+    }
+}
